df40db2224c0fef222ab3805cf8b242c04f9422d
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-03-09 09:38:06 -0600

    Commit inicial del curso

---
+++ variables/variables.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Variables
+var str = "Hello, playground"
+
+//Constantes
+let attempsOfLogin = 10
+
+var x = 0.0, y = 0.0, z = 0.0
+
+//Tipo
+var welcome:String
+welcome = "Hola"
+
+var red, green, blue: Double
+
+let π = 3.14159265
+
+let  🤑 = 4
+
+let 😀😃 = "feliz"
+
+😀😃
+
+print(1,2,3, separator:"-", terminator:".")
+
+print("Welcome: \(str)")
+
+//Números
+//UInt8: entero sin signo 8 bits
+//Int32: entero de 32 bits
+let minValue = UInt8.min
+let maxValue = UInt8.max
+
+var entero:Int = 17
+var enteroPositivo:UInt = 32
+
+print("\(INT64_MAX)")
+print("\(Int64.max)")
+
+//Literales
+let decimal = 17
+let binario = 0b10001
+let octal = 0o21
+let hexadecimal = 0x11
+let number = 1.25e2
+let number2 = 1.25e-2
+let number3 = 0xFp2
+let number4 = 0xFp-2
+let number5 = 0xC.3p0
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let overMillion = 1_000_000.000_000_1
+
+
