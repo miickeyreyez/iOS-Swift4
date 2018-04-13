aad3f0f8a5c2787adb022aec200567ed4879f909
Miguel Angel Martinez Reyes <maxr@praxis.com.mx>
2018-04-12 19:16:36 -0500

    Avance del juego aim for that, donde se hace la interacción con los elementos de la vista

--- aimForThat/AimForThat/AimForThat/ViewController.swift
+++ aimForThat/AimForThat/AimForThat/ViewController.swift
@@ -11,46 +11,85 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     var currentValue:Int = 0;
     var targetValue:Int = 0;
+    var score:Int = 0;
+    var round:Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.currentValue = lroundf(slider.value)
-        
-        //Todos los números son probables número entre 1 y 100
-        self.targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
   
     @IBAction func showAlert(_ sender: UIButton) {
-        print("He pulsado el botón")
+        
+        var difference : Int = 0
+        
+        /*if self.currentValue > self.targetValue {
+            difference = self.currentValue - self.targetValue
+        } else {
+            difference = self.targetValue - self.currentValue
+        }*/
+        difference = abs(self.currentValue - self.targetValue)
+        
+        let points = (difference > 0) ? 100 - difference : 1000
+        
+        self.score += points
+        self.scoreLabel.text = "\(self.score)"
         
         let message = """
             El valor del slider  es \(self.currentValue)
             El valor del objetivo es \(self.targetValue)
+            La diferencia es \(difference)
+            Has marcado \(points) puntos
         """
+        let title:String
+        
+        switch difference{
+        case 0:
+            title = "Puntuación perfecta"
+        case 1...5:
+            title = "Puntuación casi perfecta"
+        default:
+            title = "Intenta nuevamente"
+        }
         
         let alert = UIAlertController(title:"Hola mundo",message:message,preferredStyle:.alert)
-        
         let action = UIAlertAction(title:"Genial",style:.default,handler:nil)
         
         alert.addAction(action)
         
         present(alert,animated: true)
         
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ sender: UISlider) {
-        print("El valor del slider  es \(sender.value)")
         self.currentValue = lroundf(sender.value)
-        
+    }
+    
+    func startNewRound(){
+        //Todos los números son probables número entre 1 y 100
+        self.targetValue = 1 + Int(arc4random_uniform(100))
+        self.currentValue = lroundf(slider.value)
+        self.slider.value = Float(self.currentValue)
+        self.round += 1
+    }
+    
+    func updateLabels(){
+        self.targetLabel.text = "\(self.targetValue)"
+        self.scoreLabel.text = "\(self.score)"
+        self.roundLabel.text = "\(self.round)"
     }
 }
 
